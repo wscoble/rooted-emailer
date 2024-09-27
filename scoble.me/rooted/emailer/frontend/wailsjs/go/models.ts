c54df4226c20890c914baa9cdcd4934891fce9b2
Scott Scoble <scott@scoble.me>
2024-09-26 22:43:15 -0700

    update builds

--- scoble.me/rooted/emailer/frontend/wailsjs/go/models.ts
+++ scoble.me/rooted/emailer/frontend/wailsjs/go/models.ts
@@ -16,6 +16,26 @@ export namespace main {
 	        this.body = source["body"];
 	    }
 	}
+	export class TemplateData {
+	    Name: string;
+	    Emails: string[];
+	    Snacks: number;
+	    CurrentWeek: number;
+	    Facilitators: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new TemplateData(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Name = source["Name"];
+	        this.Emails = source["Emails"];
+	        this.Snacks = source["Snacks"];
+	        this.CurrentWeek = source["CurrentWeek"];
+	        this.Facilitators = source["Facilitators"];
+	    }
+	}
 
 }
 
